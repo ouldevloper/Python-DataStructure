0f6140dc5815b3c6e889d262da24899d23d47f2c
Abdellah Oulahyane <aitbaha48@gmail.com>
2022-08-15 05:31:02 +0300

    Add files via upload

---
+++ 7 - Tree's/inorder.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3 
+
+
+
+
+class Node:
+    def __init__(self,value=None,right=None,lift=None):
+        self.item = value 
+        self.right = right 
+        self.lift  = lift 
+    def set_right(self,right):
+        self.right=right
+    
+    def set_lift(self,lift):
+        self.lift = lift
+        
+        
+#                          10
+#                 5                  11                         
+#               6   8          12          15                                
+#              7      9     13    14    16    19                       
+#                                             20                      
+#                                                                   
+
+root = Node(10)
+
+
+n1 = Node(5)
+n2 = Node(6)
+n3 = Node(8)
+n4 = Node(7)
+n5 = Node(9)
+n6 = Node(11)
+n7 = Node(12)
+n13 = Node(13)
+n8 = Node(14)
+n9 = Node(15)
+n10 = Node(16)
+n11 = Node(19)
+n12 = Node(20)
+
+root.lift = n6
+root.right = n1
+n1.right = n2
+n1.lift = n3
+n2.right = n4
+n3.right = n5
+
+n6.right = n7
+n6.lift = n9
+n7.right = n13
+n7.lift = n8
+n9.right = n10
+n9.lift = n11
+n11.right=n12
+
+
+def inorder(root):
+    if root==None:
+        return
+    inorder(root.right)
+    print(root.item)
+    inorder(root.lift)
+    
+inorder(root)
