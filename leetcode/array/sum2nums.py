0a21f60e55822677f1ec38e01ae1b7ec30bd5343
user <user@localhost.localdomain>
2023-09-19 01:48:04 +0300

    fix non possitive numbers

--- leetcode/array/sum2nums.py
+++ leetcode/array/sum2nums.py
@@ -2,22 +2,26 @@
 
 
 class Sum2Nums:
-    def __init__(self):
-        pass
-    
-
     def getSum(self, num1, num2)->int:
+        if num1 < 0 or num2 < 0:
+            print ("Numbers shoud be a Possitive!")
+            return
+
         while num2 != 0:
-            # print()
+            print()
             carry = 0
-            # print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+            print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+
             carry = num1 & num2
-            # print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+            print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+
             num1 ^= num2
-            # print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+            print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+
             num2 = carry << 1
-            # print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
-            # print()
+            print("num1: %d, num2: %d, carry: %d, \n"%(num1, num2, carry))
+            print()
         return num1
 
-print (Sum2Nums().getSum(10, 10))
+    
+print (Sum2Nums().getSum(100, 10000))
